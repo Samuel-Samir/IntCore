8eca97107eac89b768f1d2b1574e46e7a9ad0779
samuel <samuelsamer952@gmail.com>
2017-07-11 22:10:49 +0200

    adding layout design

--- app/src/main/java/samuel/example/com/intcore/MainActivity.java
+++ app/src/main/java/samuel/example/com/intcore/MainActivity.java
@@ -2,6 +2,8 @@ package samuel.example.com.intcore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
